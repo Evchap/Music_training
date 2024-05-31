4110a8481df7110bff9a8145af5b5441bc74ecce
Evgen Chap <evchap8888@gmail.com>
2024-05-31 22:42:45 +0300

    Added pages for avtorisation.

--- Music_training/Music_training/settings.py
+++ Music_training/Music_training/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     # own
     'bootstrap4',
     'django.contrib.postgres',
