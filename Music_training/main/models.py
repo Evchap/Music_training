ad373ddb21b446a2af748d0ca38ee14f7b491082
Evgen Chap <evchap8888@gmail.com>
2024-05-31 08:11:11 +0300

    Added module "main", added database postgresql

---
+++ Music_training/main/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Models for BD
+class Subject(models.Model): # Book Django 2, page 304
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering=['title']
+
+    def __str__(self):
+        return self.title
+
+class Course(models.Model):
+    owner = models.ForeignKey(User, # преподаватель, который создал курс
+                              related_name='courses_created',
+                              on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, # предмет, к которому привязан курс
+                                related_name='courses',
+                                on_delete=models.CASCADE)
+    title = models.CharField(max_length=200) # название курса
+    slug = models.SlugField(max_length=200, unique=True) # слаг курса, будем использовать его для формирования
+                                                        # человекопонятных url
+    overview = models.TextField() # текстовое поле для создания краткого описания курса
+    created = models.DateTimeField(auto_now_add=True) # дата и время создания курса
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+class Module(models.Model):
+    course = models.ForeignKey(Course,
+                               related_name='modules',
+                               on_delete=models.CASCADE
+                               )
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
---
+++ Music_training/main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# from main.models import Post
+#
+#
+# def index(request):
+#
+#     all_posts = Post.objects.all() # objects - менеджер
+#     print("all_posts", all_posts)
+#     return render(request, "main/index.html")
+
+
+
+
